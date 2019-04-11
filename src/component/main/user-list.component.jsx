fde59d77f3cea6ac7ae81401938cc407d5b630a5
Haoyu.Li <18844144418@163.com>
2019-04-11 15:53:55 +0800

    feature(web-app-learn): set react-routerV4 get and dynamic to pass the value

--- src/component/main/user-list.component.jsx
+++ src/component/main/user-list.component.jsx
@@ -9,7 +9,7 @@ class UserListComponent extends Component {
   render() {
     return (
       <div>
-          我是预留为 user list列表的 page
+          我是预留为 user list列表的 page 我佛了
           <p>
             名字是：{this.props.match.params.name}
           </p>
