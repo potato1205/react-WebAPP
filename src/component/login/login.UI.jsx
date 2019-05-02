5f024848ebadb748ca53dbb2f4de876596a6b5c4
s_lihy <18844144418@163.com>
2019-05-02 19:04:33 +0800

    feature(webapp): login UI

--- src/component/login/login.UI.jsx
+++ src/component/login/login.UI.jsx
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 
+import './login.css';
+
+import avtar from '../../assets/images/avtar.png';
+
 class LoginUIComponent extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         console.log('loginUI: ', this.props);
         console.log('loginUI: ', props);
         // console.log('loginUI: ', this.props.name);
         // console.log('loginUI: ', props.name);
+        this.state = {
+            userNameImg : '../../assets/images/avtar.png',
+        }
     }
-
+    
     componentWillMount() {
-        // fetch('../mock/login.mock.json')
-        // .then((response) => {
-        //     console.log(response);
-        // });
     }
 
     render() {
         return (
-            <p>我是登录页</p>
+            <div>
+                <h1>klasikal Login Form</h1>
+                <div class="login-form">
+                    <div class="close"> </div>
+                    <div class="head-info">
+                        <label class="lbl-1"> </label>
+                        <label class="lbl-2"> </label>
+                        <label class="lbl-3"> </label>
+                    </div>
+                    <div class="clear"> </div>
+                    <div class="avtar">
+                        <img src={avtar} />
+                    </div>
+                    {/* <form> */}
+                    <input type="text" class="text" />
+                    <div class="key">
+                        <input type="password" />
+                    </div>
+                    {/* </form> */}
+                    <div class="signin">
+                        <input type="submit" value="Login" />
+                    </div>
+                </div>
+            </div >
         );
     }
 }
